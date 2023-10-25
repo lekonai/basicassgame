86c0e30e6036241a1e2c2b68aec87b13bbe3e093
lekonai <lekonai@users.noreply.github.com>
2023-10-25 14:33:02 +0100

    committing

--- ConsoleApp1/CH1.cs
+++ ConsoleApp1/CH1.cs
@@ -21,39 +21,76 @@ namespace BestGameEver
 
             Console.WriteLine("Are you even in control of yourself?");
             Thread.Sleep(2000);
-            immersiveText("In front of you stands a weird green man... with a weird letter branded on his head"); // introduction of a character, "the twelfth is his name"
+            immersiveText("In front of you stands a weird green man... with a weird letter branded on his head"); // introduction of a character, "the twelfth" is his name
             Console.WriteLine("");
             Thread.Sleep(2000);
             
             LSwitch(true);
             
-            immersiveText("he..he took everything from me :)");
+            immersiveText("he... he took everything from me :)");
             Console.WriteLine();
             
             LSwitch(false);
             
-            Thread.Sleep(1200);
+            Thread.Sleep(2000);
             Console.WriteLine("You don't particularly have a good feeling about this");
             
             Thread.Sleep(1200);
             Console.WriteLine("You try move away... you can't, like you PHYSICALLY can't, your legs are locked in place by some invisible force");
             
-            Thread.Sleep(1200);
-            Console.WriteLine("You are entrapped, immovable, stuck there");
-            
+            Thread.Sleep(3000);
+            Console.WriteLine("You are entrapped, paralysed and unable to do anything.");
+            Thread.Sleep(2000);
+
             LSwitch(true);
             
-            immersiveText("i'm only the other guy, no one remembers me :)");
+            immersiveText("\ni'm only the other guy, no one remembers me :)");
             Console.WriteLine("");
             Thread.Sleep(1200);
 
-            Console.WriteLine("")
+            immersiveText("i was before him ! i was first, i was THE twelfth, he was the thirteenth ! why am i not the important one ! he gets to do everything but i don't get to do anything ! :)");
+            Console.WriteLine("");
+            Thread.Sleep(1200);
+
+            LSwitch(false);
+            Console.WriteLine("He is quite obviously off his marbles, but you can't do anything except observe.");
+            Thread.Sleep(1200);
+            LSwitch(true);
+
+            immersiveText("hello there, hi, do you notice me? you may be the first to notice me. please, do you see me? :D");
+            Console.WriteLine("");
+            Thread.Sleep(1000);
+            LSwitch(false);
+            Console.WriteLine("It seems he's talking to you...");
+            Thread.Sleep(1000);
+            LSwitch(true);
+            immersiveText("you don't seem to be very talkative :(");
+            immersiveText("\nyou don't look very talkative D:");
+            immersiveText("\nyou don't happen to be very talkative :(");
+            immersiveText("\nwhy don't you talk >:(");
+            LSwitch(false);
+            Thread.Sleep(2000);
+            Console.WriteLine("He punches you in the face... for some absurd reason, completely knocking you out...");
+            Console.WriteLine("\nYou feel like you're back in control...");
+            Console.ReadKey();
+            Console.WriteLine("Everything's black around you though; you're unconscious, but for some reason you KNOW that you are...");
+            Console.ReadKey();
+            Console.WriteLine("How strange. Are you stuck again? This time you may be stuck entirely...");
+            Console.ReadKey();
+            immersiveText("Forever... alone in your own mind, never to be conscious again...");
+            Console.Write("\nENDING: he saw, he came, he conquered. the twelfth");
+            LSwitch(true);
+            Console.Write(" lives on... forever");
+            LSwitch(false);
+            Console.WriteLine("GAME CLOSING IN 8 SECONDS");
+            Thread.Sleep(8000);
+            Environment.Exit(0);
         }
         static public void immersiveText(string stringy) // to make the text look nice
         {
             for (int i = 0; i < stringy.Length; i++)
             {
-                Thread.Sleep(100);
+                Thread.Sleep(75);
                 Console.Write(stringy[i]);
             }
         }
